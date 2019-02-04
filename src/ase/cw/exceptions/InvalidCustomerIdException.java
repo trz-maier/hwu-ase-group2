2754d78c47ceb8093ced5d4dbdc2b2993f8894dd
Thomas <thomas.triffterer@googlemail.com>
2019-02-04 22:01:21 +0000

    Revert "Added template classes."
    This reverts commit c54fc19534ad465a6ab6a8d9ef1f171cde9c748d.

--- src/ase/cw/exceptions/InvalidCustomerIdException.java
+++
@@ -1,7 +0,0 @@
-package ase.cw.exceptions;
-
-/**
- * Created by User on 04.02.2019.
- */
-public class InvalidCustomerIdException extends Exception {
-}
--- src/ase/cw/exceptions/NoOrderException.java
+++
@@ -1,7 +0,0 @@
-package ase.cw.exceptions;
-
-/**
- * Created by User on 04.02.2019.
- */
-public class NoOrderException extends Exception {
-}
