7ccf8676435f4245c33daa750b9aa5b0a8e3f9cf
jathanasiou <jathanasiou@users.noreply.github.com>
2019-02-20 13:56:47 +0000

    added order exceptions as per #57
    added missing exception constructor

--- src/ase/cw/exceptions/EmptyOrderException.java
+++ src/ase/cw/exceptions/EmptyOrderException.java
@@ -4,4 +4,7 @@ package ase.cw.exceptions;
  * Created by User on 04.02.2019.
  */
 public class EmptyOrderException extends Exception {
+    public EmptyOrderException(String message) {
+        super(message);
+    }
 }
