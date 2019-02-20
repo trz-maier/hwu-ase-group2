3553e7b1518d6ca5d3c26ce757750cd371537933
jathanasiou <jathanasiou@users.noreply.github.com>
2019-02-20 19:13:42 +0000

    Improved handling of invalid customer IDs
    Added customer ID validator and exceptions in Order Controller
    
    Integrated validator to model and GUI
    
    Adjusted orders input file to match expected format

--- src/ase/cw/model/Order.java
+++ src/ase/cw/model/Order.java
@@ -1,7 +1,10 @@
 /**
- * 
+ *
  */
 package ase.cw.model;
+
+import ase.cw.control.OrderController;
+import ase.cw.exceptions.InvalidCustomerIdException;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -12,72 +15,73 @@ import java.util.List;
  *
  */
 public class Order {
-	private String customerId;
-	private Date timestamp;
-	private List<OrderItem> orderItems;
-	private Bill bill;
+    private String customerId;
+    private Date timestamp;
+    private List<OrderItem> orderItems;
+    private Bill bill;
 
-	/**
-	 * 
-	 */
-	public Order(String customerId) {
-		this(customerId, new Date());
-	}
+    /**
+     *
+     */
+    public Order(String customerId) throws InvalidCustomerIdException {
+        this(customerId, new Date());
+    }
 
-	public Order(String customerId, Date timestamp) {
-		this.customerId = customerId;
-		this.timestamp = timestamp;
-		this.orderItems = new ArrayList<OrderItem>();
-	}
+    public Order(String customerId, Date timestamp) throws InvalidCustomerIdException {
+        OrderController.validateCustomerId(customerId);
+        this.customerId = customerId;
+        this.timestamp = timestamp;
+        this.orderItems = new ArrayList<OrderItem>();
+    }
 
-	public void addOrderItem(Item item) {
-		this.orderItems.add(new OrderItem (item));
-	}
+    public void addOrderItem(Item item) {
+        this.orderItems.add(new OrderItem(item));
+    }
 
-	public void removeOrderItem(Item item) {
+    public void removeOrderItem(Item item) {
 //		this.orderItems.remove(orderItem);
-		for (OrderItem oitem : orderItems) {
-			if (oitem.getItem() == item) {
-				this.orderItems.remove(oitem);
-				break;
-			}
-		}
-	}
+        for (OrderItem oitem : orderItems) {
+            if (oitem.getItem() == item) {
+                this.orderItems.remove(oitem);
+                break;
+            }
+        }
+    }
 
-	/**
-	 * @return the customerId
-	 */
-	public String getCustomerId() {
-		return customerId;
-	}
+    /**
+     * @return the customerId
+     */
+    public String getCustomerId() {
+        return customerId;
+    }
 
-	/**
-	 * @param customerId the customerId to set
-	 */
-	public void setCustomerId(String customerId) {
-		this.customerId = customerId;
-	}
+    /**
+     * @param customerId the customerId to set
+     */
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
 
-	/**
-	 * @return the timestamp
-	 */
-	public Date getTimestamp() {
-		return timestamp;
-	}
+    /**
+     * @return the timestamp
+     */
+    public Date getTimestamp() {
+        return timestamp;
+    }
 
-	/**
-	 * @return the orderItems
-	 */
-	public List<OrderItem> getOrderItems() {
-		return orderItems;
-	}
+    /**
+     * @return the orderItems
+     */
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
 
-	/**
-	 * @return the bill
-	 */
-	public Bill getBill() {
-		return new Bill (this);
-	}
+    /**
+     * @return the bill
+     */
+    public Bill getBill() {
+        return new Bill(this);
+    }
 
 
 }
