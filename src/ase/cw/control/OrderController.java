04ac96719d23a024588fe82b488f5d6d0b88ba38
jathanasiou <jathanasiou@users.noreply.github.com>
2019-03-04 17:24:46 +0000

    added SimulationView interface and made adjustments to controller for #102

--- src/ase/cw/control/OrderController.java
+++ src/ase/cw/control/OrderController.java
@@ -1,14 +1,12 @@
 package ase.cw.control;
 
 import ase.cw.IO.FileReader;
-import ase.cw.exceptions.EmptyOrderException;
 import ase.cw.exceptions.InvalidCustomerIdException;
-import ase.cw.exceptions.NoOrderException;
-import ase.cw.gui.OrderFrame;
 import ase.cw.model.Bill;
 import ase.cw.model.Item;
 import ase.cw.model.Order;
 import ase.cw.model.OrderItem;
+import ase.cw.view.SimulationView;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -17,17 +15,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by User on 04.02.2019.
- */
+
 public class OrderController {
     private static final int EXPECTED_CUSTOMER_ID_LENGTH = 8;
-    private static String ENDLINE = System.lineSeparator();
+    private static final String ENDLINE = System.lineSeparator();
 
     private Map<String, Item> stockItems;
     private List<Order> orders;
-    private Order pendingOrder;
-    private OrderFrame orderFrame;
+    private SimulationView view; // TO BE ADDED
 
     public OrderController() {
         try {
@@ -36,10 +31,7 @@ public class OrderController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        this.orderFrame = new OrderFrame();
-        this.orderFrame.setOrderController(this);
-        this.orderFrame.setStockItems(stockItems.values().toArray(new Item[stockItems.size()]));
+        // TODO: initialize this.view
     }
 
     public static void main(String[] args) {
@@ -66,64 +58,6 @@ public class OrderController {
                     customerId, character));
         }
         return;
-    }
-
-    public void createNewOrder(String customerId) throws InvalidCustomerIdException, IllegalStateException {
-        validateCustomerId(customerId);
-
-        if (pendingOrder != null) throw new IllegalStateException("New order added while pending order exists");
-        pendingOrder = new Order(customerId);
-
-        this.updateOrderFrameBill();
-    }
-
-    private void updateOrderFrameOrderItems() {
-        List<OrderItem> itemsInOrder = pendingOrder.getOrderItems();
-        this.orderFrame.setOrderItems(itemsInOrder.toArray(new OrderItem[itemsInOrder.size()]));
-    }
-
-    private void updateOrderFrameBill() {
-        Bill bill = pendingOrder.getBill();
-        orderFrame.setOrderTotals(bill.getSubtotal(), bill.getDiscount(), bill.getTotal());
-    }
-
-    public void addItemToPendingOrder(Item itemToAdd) throws NoOrderException {
-        if (pendingOrder == null) throw new NoOrderException("No pending order found");
-        if (itemToAdd == null) throw new IllegalArgumentException("Null item added to order");
-        pendingOrder.addOrderItem(itemToAdd);
-
-        this.updateOrderFrameOrderItems();
-        this.updateOrderFrameBill();
-    }
-
-    public void removeItemFromPendingOrder(OrderItem itemToRemove) throws NoOrderException {
-        if (pendingOrder == null) throw new NoOrderException("No pending order found");
-        if (itemToRemove == null) throw new IllegalArgumentException("Removing null item from order");
-
-        List<OrderItem> itemsInOrder = pendingOrder.getOrderItems();
-        if (itemsInOrder.size() < 1) throw new IllegalStateException("Pending order doesn't have any items");
-
-        this.pendingOrder.removeOrderItem(itemToRemove.getItem());
-
-        this.updateOrderFrameOrderItems();
-        this.updateOrderFrameBill();
-    }
-
-    public void cancelPendingOrder() {
-        pendingOrder = null;
-        this.orderFrame.setOrderItems(new OrderItem[]{});
-        this.orderFrame.setOrderTotals((float) 0.0, (float) 0.0, (float) 0.0);
-    }
-
-    public void finalizePendingOrder() throws NoOrderException, EmptyOrderException {
-        if (this.pendingOrder == null) throw new NoOrderException("There is no pending order to submit");
-        if (this.pendingOrder.getOrderItems().size() < 1) throw new EmptyOrderException("Cannot submit empty order");
-
-        orderFrame.setOrderItems(new OrderItem[]{});
-        orderFrame.setOrderTotals((float) 0.0, (float) 0.0, (float) 0.0);
-        orderFrame.setBillString(pendingOrder.getBill().getBillString());
-        orders.add(pendingOrder);
-        pendingOrder = null;
     }
 
     /**
@@ -192,3 +126,4 @@ public class OrderController {
         }
     }
 }
+// TODO: Add logging
